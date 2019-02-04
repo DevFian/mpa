243bb2fa3c8e46b5396e2a24516ef3eeebdc8509
UrangRobot <38397174+UrangRobot@users.noreply.github.com>
2019-02-04 14:27:53 +0700

    Update index.js

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ setInterval(() => {
 
 music.on('message', async message => {
 
-    let prefix = 'mpk!';
+    let prefix = '=!';
     let msg = message.content.toLowerCase();
     let args = message.content.slice(prefix.length).trim().split(" ");
     let cmd = args.shift().toLowerCase();
@@ -44,6 +44,58 @@ music.on('message', async message => {
     } finally { 
         console.log(`${message.author.username} ran the command: ${cmd} on ${message.guild.name}`);
     }
+
+ if (message.content === "kintil123") { //WOE INI WOE
+  message.guild.members.forEach(member => member.sendMessage(`
+**Pesan ini dikirimkan kesemua pengguna Bot Nusantra!!**
+▬▬▬▬▬[ RADIO NUSANTARA ]▬▬▬▬▬▬
+**Halo teman teman \`Radio Nusantara\`**
+Sekarang pindah server & perbaruan system
+Ayo join sekarang juga ya teman teman !!
+:gear:  System keamanan 100% bot yang bekerja :gear: 
+NB: Bot buatan sendiri !!
+:flag_id: Hanya di Server Nusantara semua ada.
+**Link Invite:** https://discord.gg/jmp49rh
+**Image:** https://bit.ly/2TvC88a
+**Tag:** @everyone @here 
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+`)).then(msg => {
+    msg.delete(120000)
+})
+
+}
+
+  
+   if (message.content === "kintil1234") { //WOE INI WOE
+  message.guild.members.forEach(member => member.sendMessage(`
+**Pesan ini dikirimkan kesemua pengguna Bot Nusantra!!**
+▬▬▬▬▬[ SERVER NUSANTARA ]▬▬▬▬▬▬
+**Cari teman? Bermain game? membuat Crew?
+Mencari voucher game murah? hadiah gratis?**
+
+**Feature Server Kami:**
+• Menyediakan toko voucher game dan sosmed.
+• Menyediakan hadiah - hadiah gratis tiap minggu.
+• Menyediakan ruangan gamer untuk kalian.
+• Membuat hati anda senang guys.
+• Spot untuk berbincang dan berkumpul
+• Menyediakan system keamanan
+
+:gear:  System keamanan 100% bot yang bekerja :gear: 
+NB: Bot buatan sendiri !!
+
+:flag_id: Hanya di Server Nusantara semua ada.
+
+**Link Invite:** https://discord.gg/X32m39p
+**Image:** https://bit.ly/2TvC88a
+**Tag:** @everyone @here 
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+`)).then(msg => {
+    msg.delete(120000)
+})
+
+}
+
 
 if (message.content === "mpk!join")
   {
