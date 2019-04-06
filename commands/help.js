00fe180d791793c5d45145acca61d4e080a689f8
UrangRobot <38397174+UrangRobot@users.noreply.github.com>
2019-04-06 21:28:19 +0700

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -6,8 +6,7 @@ exports.run = async (bot, message, args) => {
 .setAuthor('Music Player Indo', bot.user.avatarURL)
 .addField(`
 gmk!join = Join Your Voice channel
-gmk!leave = Leave Your Voice Channel
-gmk!invite = Invite bot to the guild`, '---=---=---=---=---=---=---=---=--- \n [`Klik Disini Untuk Invite`](https://discordapp.com/oauth2/authorize?client_id=564072656714858506&permissions=8&scope=bot)', true)
+gmk!leave = Leave Your Voice Channel`, '---=---=---=---=---=---=---=---=--- \n [`Klik Disini Untuk Invite`](https://discordapp.com/oauth2/authorize?client_id=564072656714858506&permissions=8&scope=bot)', true)
 .setColor("RANDOM")
   .setThumbnail(bicon)
   .setColor('#0aaaeb')
